7e5df8315ba0f661cd7ebbcac3f7d69cbd4e5087
Arnaud Schreuder <arnaudschreuder@gmail.com>
2026-04-27 14:57:26 +0200

    Amended microservices and s3 templates and general UI features

--- backstage/templates/postgres.tf
+++
@@ -1,9 +0,0 @@
-{% if values.useDb %}
-resource "aws_db_instance" "default" {
-  allocated_storage    = 20
-  identifier           = "${{ values.name }}-db"
-  engine               = "postgres"
-  instance_class       = "db.t3.micro"
-  # ... rest of your config
-}
-{% endif %}
